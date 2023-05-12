bc9ad167b3b45418ba97adfd81e398d957e5603f
Shravani <shravanichendwankar@gmail.com>
2023-05-12 13:52:37 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const starsEl = document.querySelectorAll(".fa-star");
+const emojisEl = document.querySelectorAll(".fa-regular")
+const colorsArray = ["red", "orange", "lightblue", "lightgreen", "green"];
+
+updateRating(0)
+
+console.log(starsEl);
+
+starsEl.forEach((starsEl, index) => {
+    starsEl.addEventListener("click", () => {
+        updateRating(index)
+    });
+});
+
+function updateRating(index) {
+    starsEl.forEach((starEl, idx) => {
+        if (idx < index + 1) {
+            starEl.classList.add("active");
+        } else {
+            starEl.classList.remove("active");
+        }
+    });
+
+    emojisEl.forEach((emojiEl) => {
+        emojiEl.style.transform = `translateX(-${index * 50}px)`;
+        emojiEl.style.color = colorsArray[index];
+    });
+}
